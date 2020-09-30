cf5f1fe18b95855b1b0f1341bdd41361da7ee295
MatthieuMuratore <matthieu.muratore@gmail.com>
2020-09-30 16:59:11 +0200

    add administrator user status list

--- src/app/modules/administrator/table/table-datasource.ts
+++ src/app/modules/administrator/table/table-datasource.ts
@@ -3,46 +3,20 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 
-// TODO: Replace this with your own data model type
-export interface TableItem {
-  id: number;
-  goTag: string;
-  email: string;
-}
 
-// TODO: replace this with real data from your application
-const EXAMPLE_DATA: TableItem[] = [
-  {id: 1, goTag: 'Hydrogen', email: 'Hydroge@Solary.com'},
-  {id: 2, goTag: 'Hydrogen', email: 'Hydroge@Solary.com'},
-  {id: 3, goTag: 'Hydrogen', email: 'Hydroge@Solary.com'},
-  {id: 4, goTag: 'Hydrogen', email: 'Hydroge@Solary.com'},
-  {id: 5, goTag: 'Hydrogen', email: 'Hydroge@Solary.com'},
-  {id: 6, goTag: 'Hydrogen', email: 'Hydroge@Solary.com'},
-];
-
-/**
- * Data source for the Table view. This class should
- * encapsulate all logic for fetching and manipulating the displayed data
- * (including sorting, pagination, and filtering).
- */
-export class TableDataSource extends DataSource<TableItem> {
-  data: TableItem[] = EXAMPLE_DATA;
+export class TableDataSource extends DataSource<User> {
+  data: User[] = [];
   paginator: MatPaginator;
   sort: MatSort;
 
-  constructor() {
+  constructor(data: User[]) {
     super();
+    this.data = data;
   }
 
-  /**
-   * Connect this data source to the table. The table will only update when
-   * the returned stream emits new items.
-   * @returns A stream of the items to be rendered.
-   */
-  connect(): Observable<TableItem[]> {
-    // Combine everything that affects the rendered data into one update
-    // stream for the data-table to consume.
+  connect(): Observable<User[]> {
     const dataMutations = [
       observableOf(this.data),
       this.paginator.page,
@@ -54,26 +28,14 @@ export class TableDataSource extends DataSource<TableItem> {
     }));
   }
 
-  /**
-   *  Called when the table is being destroyed. Use this function, to clean up
-   * any open connections or free any held resources that were set up during connect.
-   */
   disconnect() {}
 
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getPagedData(data: TableItem[]) {
+  private getPagedData(data: User[]) {
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     return data.splice(startIndex, this.paginator.pageSize);
   }
 
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getSortedData(data: TableItem[]) {
+  private getSortedData(data: User[]) {
     if (!this.sort.active || this.sort.direction === '') {
       return data;
     }
@@ -81,15 +43,17 @@ export class TableDataSource extends DataSource<TableItem> {
     return data.sort((a, b) => {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
-        case 'id': return compare(a.id, b.id, isAsc);
+        case 'id': return compare(a.userId, b.userId, isAsc);
         case 'goTag': return compare(a.goTag, b.goTag, isAsc);
+        case 'active': return compare(a.isActive, b.isActive, isAsc); 
+        case 'ban': return compare(a.isBan, b.isBan, isAsc); 
         default: return 0;
       }
     });
   }
 }
 
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-function compare(a: string | number, b: string | number, isAsc: boolean) {
+function compare(a: string | number | boolean, b: string | number| boolean, isAsc: boolean) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
+
--- src/app/modules/administrator/table/table.component.ts
+++ src/app/modules/administrator/table/table.component.ts
@@ -1,30 +1,118 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { TableDataSource, TableItem } from './table-datasource';
+import { first } from 'rxjs/operators';
+import { ConfirmBoxComponent } from 'src/app/components/confirm-box/confirm-box.component';
+import { User } from 'src/app/models/user.model';
+import { UserError } from 'src/app/models/userError.enum';
+import { UserService } from 'src/app/services/user.service';
+import { TableDataSource } from './table-datasource';
 
 @Component({
   selector: 'administrator-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements AfterViewInit, OnInit {
+export class TableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatTable) table: MatTable<TableItem>;
+  @ViewChild(MatTable) table: MatTable<User>;
+  
   dataSource: TableDataSource;
-
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'goTag', 'email', 'actions'];
+  displayedColumns = ['id', 'goTag', 'email', 'active', 'ban', 'delete'];
 
-  ngOnInit() {
-    this.dataSource = new TableDataSource();
-  }
+
+  constructor(
+    private snackbar: MatSnackBar,
+    private matDialog: MatDialog,
+    private userService: UserService,
+  ) {  }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    this.userService.getAll()
+          .pipe(first())
+          .subscribe((data) => {
+            this.dataSource = new TableDataSource(data);
+            this.dataSource.sort = this.sort;
+            this.dataSource.paginator = this.paginator;
+            this.table.dataSource = this.dataSource;
+          },
+          error => {
+            this.getServerErrorMessage(error);
+          });
   }
+
+  delete(id :number) : void {
+    const dialogConfig = new MatDialogConfig();
+    let dialogRef = this.matDialog.open(ConfirmBoxComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(confirm => {
+      if(confirm) {
+        let index = this.dataSource.data.findIndex(x => x.userId == id);
+        this.dataSource.data[index].isRequesting = true;
+        this.userService.delete(id)
+          .pipe(first())
+          .subscribe(() => {
+            this.dataSource.data[index].isRequesting = false;
+            this.dataSource.data.splice(index, 1);
+            this.dataSource = new TableDataSource(this.dataSource.data);
+            this.dataSource.sort = this.sort;
+            this.dataSource.paginator = this.paginator;
+            this.table.dataSource = this.dataSource;
+          },
+          error => {      
+            this.dataSource.data[index].isRequesting = false;    
+            this.getServerErrorMessage(error);
+          });
+      } 
+    })
+  }
+
+  activate(id :number) : void {
+    let index = this.dataSource.data.findIndex(x => x.userId == id);
+    this.dataSource.data[index].isRequesting = true;
+    this.userService.activate(id)
+      .pipe(first())
+      .subscribe( () => {
+          this.dataSource.data[index].isRequesting = false;
+          this.dataSource.data[index].isActive = !this.dataSource.data[index].isActive;
+        },
+        error => {
+          this.dataSource.data[index].isRequesting = false;
+          this.getServerErrorMessage(error);
+        })
+  }
+
+  ban(id :number) : void {
+    let index = this.dataSource.data.findIndex(x => x.userId == id);
+    this.dataSource.data[index].isRequesting = true;
+    this.userService.ban(id)
+      .pipe(first())
+      .subscribe( () => {
+          this.dataSource.data[index].isRequesting = false;
+          this.dataSource.data[index].isBan = !this.dataSource.data[index].isBan;
+        },
+        error => {
+          this.dataSource.data[index].isRequesting = false;
+          this.getServerErrorMessage(error);
+        })
+  }
+
+  private getServerErrorMessage(httpError: HttpErrorResponse) {
+    let msg : string;
+    if(httpError.status == 404) {
+      if(httpError.error.type == UserError.NotFound)
+        msg = 'invalid login or password';
+    }
+    else
+      msg = 'Unable to connect to server';
+    if(msg)
+      this.snackbar.open(msg, 'Dismiss', {
+        duration: 3000
+      });
+  }    
 }
